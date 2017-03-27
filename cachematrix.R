8ef5e60c44bf872c6d0f60a1dfaff589cc22624d
SilviaYarim <silviaybv@gmail.com>
2017-03-27 00:12:55 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##This function creates a special matrix object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+        ## x:square invertible matrix
+        
+        inv = NULL
+        set = function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get = function()x
+        setinv = function(inverse) inv <<- inverse
+        getinv = function() inv
+        
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+        
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function computes the inverse of the special matrix returned by makeCacheMatrix.
+cacheSolve <- function(x,...){
+        ## x: output of makeCacheMatrix()
+        ## return the inverse of the original matrix input to makeCacheMatrix()
+        
+        inv = x$getinv()
+        
+        #if the inverse has already been calculated
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        mat.inv = x$get()
+        inv = solve(mat.inv,...)
+        
+        x$setinv(inv)
+        return(inv)
+        
 }
+    
+spematrix<- makeCacheMatrix(matrix(1:4,2,2))
+> spematrix$getinv()
+NULL
+> cacheSolve(spematrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(spematrix)
+getting cached data
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+                
+                
